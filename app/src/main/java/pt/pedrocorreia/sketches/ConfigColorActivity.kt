0df4cb9a9b00dae834be83a0a9e7682283266072
Pedro Correia <pedrogrcorreia@gmail.com>
2022-10-20 17:01:38 +0100

    creates the configuration color activity. updates the screen on seekbar changes

--- app/src/main/java/pt/pedrocorreia/sketches/ConfigColorActivity.kt
+++ app/src/main/java/pt/pedrocorreia/sketches/ConfigColorActivity.kt
@@ -1,11 +1,57 @@
 package pt.pedrocorreia.sketches
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import pt.pedrocorreia.sketches.databinding.ActivityConfigColorBinding
+import pt.pedrocorreia.sketches.databinding.ActivityMainBinding
 
 class ConfigColorActivity : AppCompatActivity() {
+    lateinit var binding: ActivityConfigColorBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_config_color)
+        binding = ActivityConfigColorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // Red bar
+        binding.seekRed.max = 255
+        binding.seekRed.progress = 255
+        binding.seekRed.setOnSeekBarChangeListener(procSeekBarChange)
+
+        // Green bar
+        binding.seekGreen.max = 255
+        binding.seekGreen.progress = 255
+        binding.seekGreen.setOnSeekBarChangeListener(procSeekBarChange)
+
+        // Blue bar made with scope function to demo
+        binding.seekBlue.run {
+            max = 255
+            progress = 255
+            setOnSeekBarChangeListener(procSeekBarChange)
+        }
+
+        updateView()
+    }
+
+    private val procSeekBarChange = object : SeekBar.OnSeekBarChangeListener {
+        override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+            updateView()
+        }
+
+        override fun onStartTrackingTouch(p0: SeekBar?) {
+
+        }
+
+        override fun onStopTrackingTouch(p0: SeekBar?) {
+
+        }
+    }
+
+    fun updateView(){
+        var color = Color.rgb(binding.seekRed.progress, binding.seekGreen.progress, binding.seekBlue.progress)
+        binding.frPreview.setBackgroundColor(color)
     }
 }
