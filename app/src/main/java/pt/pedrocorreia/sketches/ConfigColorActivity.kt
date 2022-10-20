2d4e55e4ab86dc825866d8db23cbec81693dfd31
Pedro Correia <pedrogrcorreia@gmail.com>
2022-10-20 16:36:50 +0100

    Button processing. Started new activity

---
+++ app/src/main/java/pt/pedrocorreia/sketches/ConfigColorActivity.kt
@@ -0,0 +1,11 @@
+package pt.pedrocorreia.sketches
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfigColorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_config_color)
+    }
+}
--- app/src/main/java/pt/pedrocorreia/sketches/MainActivity.kt
+++ app/src/main/java/pt/pedrocorreia/sketches/MainActivity.kt
@@ -1,11 +1,41 @@
 package pt.pedrocorreia.sketches
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import pt.pedrocorreia.sketches.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val btnSolidColor : Button = findViewById(R.id.solidColor)
+        val btnGallery : Button = findViewById(R.id.gallery)
+        val btnPhoto : Button = findViewById(R.id.photo)
+        val btnHistory : Button = findViewById(R.id.history)
+        btnSolidColor.setOnClickListener {
+            intent = Intent(this, ConfigColorActivity::class.java)
+            startActivity(intent)
+        }
+        btnGallery.setOnClickListener(makeSnackbar)
+        btnPhoto.setOnClickListener(this)
+        btnHistory.setOnClickListener(makeSnackbar)
+    }
+
+    // Function to retrieve a toast on a button click
+    override fun onClick(p0: View?) {
+        val btn = p0 as Button
+        Toast.makeText(this,"${btn.text}: ${getString(R.string.todo)}", Toast.LENGTH_LONG).show()
+    }
+
+    private val makeSnackbar = View.OnClickListener {
+        Snackbar.make(it, "${(it as Button).text}: ${getString(R.string.todo)}", Snackbar.LENGTH_LONG).show()
     }
 }
