444aaa95f785d4a8a1d4c030995f57fac54edb66
Pedro Correia <pedrogrcorreia@gmail.com>
2022-11-03 15:39:02 +0000

    ask permission when pressing photo or gallery buttons

--- app/src/main/java/pt/pedrocorreia/sketches/ConfigImageActivity.kt
+++ app/src/main/java/pt/pedrocorreia/sketches/ConfigImageActivity.kt
@@ -2,15 +2,19 @@ package pt.pedrocorreia.sketches
 
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import pt.pedrocorreia.sketches.databinding.ActivityConfigImageBinding
 import pt.pedrocorreia.sketches.databinding.ActivityDrawingAreaBinding
 
 class ConfigImageActivity : AppCompatActivity() {
 
     companion object {
+        private const val PERMISSION_REQUEST_CODE = 10
         private const val GALLERY = 1
         private const val CAMERA = 2
         private const val MODE_KEY = "mode"
@@ -31,6 +35,7 @@ class ConfigImageActivity : AppCompatActivity() {
     private var mode = GALLERY
 
     lateinit var binding: ActivityConfigImageBinding
+    private var permissionsGranted = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,6 +57,42 @@ class ConfigImageActivity : AppCompatActivity() {
             }
             else -> finish()
         }
+
+        verifyPermissions()
+    }
+
+    private fun verifyPermissions(){
+        if ( ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        )  {
+            permissionsGranted = false
+            ActivityCompat.requestPermissions(this,
+                arrayOf(
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ),
+                PERMISSION_REQUEST_CODE
+            )
+        } else {
+            permissionsGranted = true
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            permissionsGranted = grantResults.all { it == PackageManager.PERMISSION_GRANTED}
+            return
+        }
+        super.onRequestPermissionsResult(
+            requestCode, permissions, grantResults)
     }
 
     private fun chooseImage(){
